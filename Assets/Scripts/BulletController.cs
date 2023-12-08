e979d9d3afc58a0d3c7e37d0d7b1220e5c237703
Yuriy Petrov <Jurijp2005@gmail.com>
2023-12-08 17:16:32 +0100

    commit3?

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-   Rigidbody2D Bulletrigidbody;
+    Rigidbody2D Bulletrigidbody;
 
-   [SerializeField]
-   float Bulletspeed;
+    [SerializeField]
+    float Bulletspeed;
     void Start()
     {
         Bulletrigidbody = GetComponent<Rigidbody2D>();
-        Destroy(gameObject, t:3f); //objekt raderas efter 4 sek. https://discussions.unity.com/t/destroy-object-after-a-delay/35759/5
+        Destroy(gameObject, t: 3f); //objekt raderas efter 4 sek. https://discussions.unity.com/t/destroy-object-after-a-delay/35759/5
     }
 
     void Update()
     {
-        Bulletrigidbody.velocity = (transform.up * Bulletspeed); // Jag säger att bullet ska röra sig med riktningen uppåt det vil säga spritens up.
+        Bulletrigidbody.velocity = transform.up * Bulletspeed; // Jag säger att bullet ska röra sig med riktningen uppåt det vil säga spritens up.
 
-        
+
     }
 
-    private void OnCollisionEnter2D(Collision2D other) 
+    private void OnCollisionEnter2D(Collision2D other)
     {
         var enemy = other.collider.GetComponent<ZombieController>();
-
-        if (enemy != null) //när kolliderar med fiende då raderas spel objekt.
+        if (other.gameObject.tag == "Enemy") //specifierar att dne ska kollidera just med tag enemy. alltså att other är enemy.
         {
             enemy.Death();
+            Destroy(gameObject); //raderar bullet när den kollideras med något
         }
 
-        //Destroy(gameObject); //raderar bullet när den kollideras med något
+        //if (enemy != null) //när kolliderar med fiende då raderas spel objekt.
+
+
 
     }
 }
--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -1,27 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ZombieController : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
-    GameObject _player;
+    private GameObject _player;
 
     float Randomspeed;    //ska göra en randomizer för att hastigheten skulle varieras.
 
     [SerializeField]
-    float Zombiespeed;  //bas hastighet.
+    float Zombiespeed = 5;  //bas hastighet.
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         Randomspeed = Zombiespeed * (1 + Random.Range(-0.1f, 0.1f)); // bas hastighet * belopp mellan -0.1 och 0.1. Vilket betyder hastighet varierar inom 10% här källa https://gamedevbeginner.com/how-to-use-random-values-in-unity-with-examples/
-        _player = FindObjectOfType<GameObject>();  //dåligt att använda ifall jag har flera GameObject. Men tror det funkar utan problem?
+        _player = GameObject.FindGameObjectWithTag("Player");  //dåligt att använda ifall jag har flera GameObject. Men tror det funkar utan problem? behövs för att ge referens till player
     }
 
     void Update()
     {
         transform.up = (_player.transform.position - transform.position).normalized; // samma sak som transform.up = (LookAtMouse.GetMousePosition2d() - (Vector2)transform.position).normalized;
-        _rigidbody.velocity = (transform.up * Zombiespeed);  //samma sak som _rigidbody.velocity = _input.normalized * Speed; så återanvänder kod
+         _rigidbody.velocity = transform.up * Zombiespeed;  //samma sak som _rigidbody.velocity = _input.normalized * Speed; så återanvänder kod
+         
     }
 
     public void Death()
